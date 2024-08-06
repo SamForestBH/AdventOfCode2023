14d85104d14e2a3a9cde61db4478d1e98383d444
smuriello <smuriello@G067-114178.nr.edu>
2024-08-06 11:09:21 -0400

    Day 6 (boats) done w/ comments

---
+++ src/day6/BoatPart1.java
@@ -0,0 +1,60 @@
+package day6;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class BoatPart1 {
+
+	public static void main(String[] args) throws IOException {
+		//Reads line from input file
+		File input = new File("day6\\input.txt");
+		BufferedReader br = new BufferedReader(new FileReader(input));
+		String line;
+		int count = 0;
+		//Takes input file and creates time and distance array. 
+		line = br.readLine();
+		String[] tempa = line.split("\\s+");
+		String[] stimes = Arrays.copyOfRange(tempa, 1, tempa.length);
+		long[] times = new long[stimes.length];
+		for(int i = 0; i < stimes.length; i++)
+		{
+			times[i] = Long.parseLong(stimes[i]);
+		}
+		line = br.readLine();
+		tempa = line.split("\\s+");
+		String[] sdistances = Arrays.copyOfRange(tempa, 1, tempa.length);
+		long[] distances = new long[stimes.length];
+		for(int i = 0; i < sdistances.length; i++)
+		{
+			distances[i] = Long.parseLong(sdistances[i]);
+		}
+		//Using quadratic formula, computes max and min times. 
+		long product = 1;
+		long[] wincounts = new long[times.length];
+		for (int i = 0; i < times.length; i++)
+		{
+			long t1 = (long) Math.max(0,  Math.ceil((times[i] - Math.sqrt(times[i]*times[i] - 4*distances[i]))/2));
+			//Offset if min time is a tie
+			if (t1 * (times[i] - t1) == distances[i])
+			{
+				t1 ++;
+			}
+			//Offset if max time is a tie
+			long t2 = (long) Math.min(times[i],  Math.floor((times[i] + Math.sqrt(times[i]*times[i] - 4*distances[i]))/2));
+			if (t2 * (times[i] - t2) == distances[i])
+			{
+				t2 --;
+			}
+			System.out.println("Win by pressing between " + t1 + " and " + t2 + " for a total of " + (t2-t1) + " ways to win. ");
+			product *= (t2 - t1 + 1);
+		}
+		System.out.println("Win product: " + product);
+	}
+}
---
+++ src/day6/BoatPart2.java
@@ -0,0 +1,55 @@
+package day6;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class BoatPart2 {
+
+	public static void main(String[] args) throws IOException {
+		//Reads line from input file
+		File input = new File("day6\\input.txt");
+		BufferedReader br = new BufferedReader(new FileReader(input));
+		String line;
+		int count = 0;
+		//Takes input file and creates time and distance array. 
+		line = br.readLine();
+		String[] tempa = line.split("\\s+");
+		String[] stimes = Arrays.copyOfRange(tempa, 1, tempa.length);
+		String stime = "";
+		for(int i = 0; i < stimes.length; i++)
+		{
+			stime += stimes[i];
+		}
+		long time = Long.parseLong(stime);
+		line = br.readLine();
+		tempa = line.split("\\s+");
+		String[] sdistances = Arrays.copyOfRange(tempa, 1, tempa.length);
+		String sdistance = "";
+		for(int i = 0; i < sdistances.length; i++)
+		{
+			sdistance += sdistances[i];
+		}
+		long distance = Long.parseLong(sdistance);
+		//Using quadratic formula, computes max and min times. 
+		long t1 = (long) Math.max(0,  Math.ceil((time - Math.sqrt(time*time - 4*distance))/2));
+		//Offset if min time is a tie
+		if (t1 * (time - t1) == distance)
+		{
+			t1 ++;
+		}
+		//Offset if max time is a tie
+		long t2 = (long) Math.min(time,  Math.floor((time + Math.sqrt(time*time - 4*distance))/2));
+		if (t2 * (time - t2) == distance)
+		{
+			t2 --;
+		}
+		System.out.println("Win by pressing between " + t1 + " and " + t2 + " for a total of " + (t2-t1 + 1) + " ways to win. ");
+	}
+}
